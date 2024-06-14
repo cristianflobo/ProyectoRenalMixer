829c8a926561ce28667d17525222c40621e9e793
cristian lobo <cristianflobo@hotmail.com>
2024-06-14 17:00:03 -0500

    se cambia recirculacion

--- src/renderer/src/hooks/useApp.tsx
+++ src/renderer/src/hooks/useApp.tsx
@@ -237,7 +237,7 @@ const useApp = () => {
           </button>
         </div>
       ),
-      procesoGpio: ['valvula 2', 'valvula 3', 'bomba 1']
+      procesoGpio: ['valvula 2', 'bomba 1']
     }],
     transferir:
    [ { //BOTON TRANSFERIR
